354458a12e6e14c7aa5ff9d7220a56175de686af
fikriaplubis <fikriaplubis@gmail.com>
2022-09-28 13:39:11 +0700

    add method POST

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,6 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
-export default function Home() { 
+export default function Home() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   useEffect(() => {
@@ -18,17 +18,51 @@ export default function Home() {
     }
   }
 
-  if (error) {
-    return <div>Failed to load {error.toString()}</div>
-  }
+  return (
+    <div>
+      {error && <div>Failed to load {error.toString()}</div>}
+      {!data ? <div> Loading... </div> : ((data?.data ?? []).length === 0 && <p>data kosong</p>)}
+      <Input onSuccess={getData} />
+      {data?.data && data?.data?.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
+    </div>
+  )
+}
 
-  if (!data) {
-    return <div>Loading...</div>
+function Input({ onSuccess }) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const body = {
+      text: formData.get("data")
+    }
+
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/send`, {
+        method: 'POST',
+        body: JSON.stringify(body)
+      });
+      const data = await res.json();
+      setData(data.message);
+      onSuccess();
+    }
+    catch (error) {
+      setError(error);
+    }
   }
 
   return (
     <div>
-      <p>data: {data.message}</p>
+      {error && <p>error: {error.toString()}</p>}
+      {data && <p>success: {data}</p>}
+      <form onSubmit={handleSubmit}>
+        <input name="data" type="text" />
+        <button >Submit</button>
+      </form>
     </div>
   )
 }
